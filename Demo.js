df86dfbc860ade7f10fbe873938f39c0f5bdd353
Sahil Dhote <136931176+sd0233@users.noreply.github.com>
2023-09-22 00:03:40 +0530

    Add files via upload

---
+++ Demo.js
@@ -0,0 +1,32 @@
+// let demo = document.querySelector(["data-temp"]);
+
+// function getLocation() {
+//     if(navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     }
+//     else {
+//         //HW - show an alert for no gelolocation support available
+//         console.log("GEOLOCATIN NOT AVAILABLE ");
+//     }
+// }
+
+// function showPosition(position) {
+
+   
+//        let lat = position.coords.latitude;
+//        let lon = position.coords.longitude;
+
+//     //    document.write(lat);
+
+//     //    document.write(lon);
+//         // demo.innerText = lat;
+//         // demo.innerText = lon;
+//         window.alert("Latidude"+lat);
+//         window.alert("Longitude"+lon);
+
+
+    
+
+// }
+
+// getLocation();
